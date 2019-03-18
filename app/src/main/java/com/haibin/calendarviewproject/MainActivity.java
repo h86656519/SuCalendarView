50ccb6e5f770b58b509f29eabab9af5a1f260318
h86656519 <h86656519@gmail.com>
2019-03-18 16:36:22 +0800

    ：wq

--- app/src/main/java/com/haibin/calendarviewproject/MainActivity.java
+++ app/src/main/java/com/haibin/calendarviewproject/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends BaseActivity implements
         View.OnClickListener {
 
     TextView mTextMonthDay;
-//999
+//99
     TextView mTextYear;
 
     TextView mTextLunar;
